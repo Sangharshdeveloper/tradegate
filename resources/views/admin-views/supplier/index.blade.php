253cfc7f6fcec895fa86c630999d2332ffc08006
sangharshdeveloper <sangharshdeveloper@gmail.com>
2024-10-17 21:54:22 +0530

    change layout in suppliers

--- resources/views/admin-views/supplier/index.blade.php
+++ resources/views/admin-views/supplier/index.blade.php
@@ -12,6 +12,80 @@
                 <span class="badge badge-soft-dark radius-50 fz-12">{{ $suppliers->total() }}</span>
             </h2>
         </div>
+
+        <div class="row">
+            <section class="col-lg-12">
+                @if(count($suppliers) > 0)
+                    <div class="row mx-n2 __min-h-200px">
+                        @foreach ($suppliers as $vendorItem)
+                            @php($current_date = date('Y-m-d'))
+                            @php($start_date = date('Y-m-d', strtotime($vendorItem['vacation_start_date'])))
+                            @php($end_date = date('Y-m-d', strtotime($vendorItem['vacation_end_date'])))
+                            <div class="col-lg-3 col-md-6 col-sm-12 px-2 pb-4 text-center">
+                                <a href="{{route('shopView',['id' => $vendorItem['id']])}}" class="others-store-card text-capitalize">
+                                    <div class="overflow-hidden other-store-banner">
+                                        <img class="w-100 h-100 object-cover" alt="" src="{{ getStorageImages(path: $vendorItem->banner_full_url, type: 'shop-banner') }}">
+                                    </div>
+                                    <div class="name-area">
+                                        <div class="position-relative">
+                                            <div class="overflow-hidden other-store-logo rounded-full">
+                                                <img class="rounded-full" alt="{{ translate('store') }}"
+                                                     src="{{ getStorageImages(path: $vendorItem->image_full_url, type: 'shop') }}">
+                                            </div>
+
+                                            @if($vendorItem['temporary_close'])
+                                                <span class="temporary-closed position-absolute text-center rounded-full p-2">
+                                                    <span>{{translate('Temporary_OFF')}}</span>
+                                                </span>
+                                            @elseif(($vendorItem['vacation_status'] && ($current_date >= $vendorItem['vacation_start_date']) && ($current_date <= $vendorItem['vacation_end_date'])))
+                                                <span class="temporary-closed position-absolute text-center rounded-full p-2">
+                                                    <span>{{translate('closed_now')}}</span>
+                                                </span>
+                                            @endif
+                                        </div>
+                                        <div class="info pt-2">
+                                            <h5 class="text-start">{{ $vendorItem['name'] }}</h5>
+                                            <div class="d-flex align-items-center">
+                                                <h6 class="web-text-primary">{{number_format($vendorItem['average_rating'],1)}}</h6>
+                                                <i class="tio-star text-star mx-1"></i>
+                                                <small>{{ translate('rating') }}</small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="info-area">
+                                        <div class="info-item">
+                                            <h6 class="web-text-primary">{{$vendorItem['review_count'] < 1000 ? $vendorItem['review_count'] : number_format($vendorItem['review_count']/1000 , 1).'K'}}</h6>
+                                            <span>{{ translate('reviews') }}</span>
+                                        </div>
+                                        <div class="info-item">
+                                            <h6 class="web-text-primary">{{$vendorItem['products_count'] < 1000 ? $vendorItem['products_count'] : number_format($vendorItem['products_count']/1000 , 1).'K'}}</h6>
+                                            <span>{{ translate('products') }}</span>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        @endforeach
+                    </div>
+
+                    <div class="row mx-n2">
+                        <div class="col-md-12">
+                            <div class="text-center">
+                                {{ $suppliers->links() }}
+                            </div>
+                        </div>
+                    </div>
+                @else
+                    <div class="mb-5 text-center text-muted">
+                        <div class="d-flex justify-content-center my-2">
+                            <img alt="" src="{{ theme_asset(path: 'public/assets/front-end/img/media/seller.svg') }}">
+                        </div>
+                        <h4 class="text-muted">{{ translate('vendor_not_available') }}</h4>
+                        <p>{{ translate('Sorry_no_data_found_related_to_your_search') }}</p>
+                    </div>
+                @endif
+            </section>
+        </div>
+
         <div class="row mt-4">
             <div class="col-md-12">
                 <div class="card">
