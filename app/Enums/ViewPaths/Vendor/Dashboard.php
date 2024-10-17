2609b7e9d42e4ab9729572d8fc6f3eb6cdf23ca8
sangharshdeveloper <sangharshdeveloper@gmail.com>
2024-10-17 18:03:39 +0530

    Suppliers route changes

--- app/Enums/ViewPaths/Vendor/Dashboard.php
+++ app/Enums/ViewPaths/Vendor/Dashboard.php
@@ -19,7 +19,7 @@ enum Dashboard
         VIEW => 'vendor-views.dashboard.partials.earning-statistics'
     ];
     const WITHDRAW_REQUEST = [
-            URI => 'withdraw-request',
+        URI => 'withdraw-request',
         VIEW => ''
     ];
     const METHOD_LIST = [
@@ -27,4 +27,55 @@ enum Dashboard
         VIEW => ''
     ];
 
+
+    //  Supplier Start
+    const INDEX_SUPPLIER = [
+        URI => '/',
+        VIEW => 'vendor-views.dashboard.index',
+        ROUTE => 'vendor.dashboard.index'
+    ];
+
+    const ORDER_STATUS_SUPPLIER = [
+        URI => 'order-status',
+        VIEW => 'supplier-views.partials._dashboard-order-status'
+    ];
+    const EARNING_STATISTICS_SUPPLIER = [
+        URI => 'earning-statistics',
+        VIEW => 'supplier-views.dashboard.partials.earning-statistics'
+    ];
+    const WITHDRAW_REQUEST_SUPPLIER = [
+        URI => 'withdraw-request',
+        VIEW => ''
+    ];
+    const METHOD_LIST_SUPPLIER = [
+        URI => 'method-list',
+        VIEW => ''
+    ];
+
+    // Suppliers End
+
+    //  Dropshipper Start
+    const INDEX_DROPSHIPPER = [
+        URI => '/',
+        VIEW => 'drophipper-views.dashboard.index',
+        ROUTE => 'drophipper.dashboard.index'
+    ];
+
+    const ORDER_STATUS_DROPSHIPPER = [
+        URI => 'order-status',
+        VIEW => 'drophipper-views.partials._dashboard-order-status'
+    ];
+    const EARNING_STATISTICS_DROPSHIPPER = [
+        URI => 'earning-statistics',
+        VIEW => 'drophipper-views.dashboard.partials.earning-statistics'
+    ];
+    const WITHDRAW_REQUEST_DROPSHIPPER = [
+        URI => 'withdraw-request',
+        VIEW => ''
+    ];
+    const METHOD_LIST_DROPSHIPPER = [
+        URI => 'method-list',
+        VIEW => ''
+    ];
+    // Dropshipper End
 }
