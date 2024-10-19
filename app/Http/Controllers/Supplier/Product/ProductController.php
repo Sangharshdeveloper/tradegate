b384afaae65b78ebdbaa77ae235909274eb036e4
sangharshdeveloper <sangharshdeveloper@gmail.com>
2024-10-19 15:36:47 +0530

    fortesting

--- app/Http/Controllers/Supplier/Product/ProductController.php
+++ app/Http/Controllers/Supplier/Product/ProductController.php
@@ -628,7 +628,7 @@ class ProductController extends BaseController
             'added_by' => 'seller',
             'searchValue' => $searchValue,
             'request_status' => 1,
-            'seller_id' => $vendorId,
+            // 'seller_id' => $vendorId,
             'brand_id' => $request['brand_id'],
             'category_id' => $request['category_id'],
         ];
