58de619dcde719fc623c3285719b28ca7c98a53a
sangharshdeveloper <sangharshdeveloper@gmail.com>
2024-10-19 08:34:26 +0530

    debug 2

--- app/Http/Controllers/Admin/Product/ProductController.php
+++ app/Http/Controllers/Admin/Product/ProductController.php
@@ -169,7 +169,7 @@ class ProductController extends BaseController
     public function getListView(Request $request, string $type): View
     {
 
-        dd($request);
+      
 
         $filters = [
             'added_by' => $type,
@@ -180,10 +180,10 @@ class ProductController extends BaseController
             'sub_category_id' => $request['sub_category_id'],
             'sub_sub_category_id' => $request['sub_sub_category_id'],
         ];
-
+       
 
         $products = $this->productRepo->getListWhere(orderBy: ['id' => 'desc'], searchValue: $request['searchValue'], filters: $filters, dataLimit: getWebConfig(name: WebConfigKey::PAGINATION_LIMIT));
-       
+        dd($products);
         $sellers = $this->sellerRepo->getByStatusExcept(status: 'pending', relations: ['shop'], paginateBy: getWebConfig(name: WebConfigKey::PAGINATION_LIMIT));
         $brands = $this->brandRepo->getListWhere(filters: ['status' => 1], dataLimit: 'all');
         $categories = $this->categoryRepo->getListWhere(filters: ['position' => 0], dataLimit: 'all');
