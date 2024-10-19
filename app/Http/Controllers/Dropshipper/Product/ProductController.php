ba6185cc61a25c2dc06996abfa722daa6fa2cd71
sangharshdeveloper <sangharshdeveloper@gmail.com>
2024-10-19 15:38:50 +0530

    now check

--- app/Http/Controllers/Dropshipper/Product/ProductController.php
+++ app/Http/Controllers/Dropshipper/Product/ProductController.php
@@ -629,7 +629,7 @@ class ProductController extends BaseController
         $filters = [
             // 'searchValue' => $searchValue,
             // 'request_status' => 1,
-            // // 'seller_id' => $vendorId,
+            'seller_id' => $vendorId,
             // 'brand_id' => $request['brand_id'],
             // 'category_id' => $request['category_id'],
             'type' => '2',
