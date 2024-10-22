e4b0f33d75c335efd3d368687232c4aa4ba7fff4
sangharshdeveloper <sangharshdeveloper@gmail.com>
2024-10-22 14:07:23 +0530

    change dropshipper dashboard order routes

--- resources/views/dropshipper-views/partials/_dashboard-order-status.blade.php
+++ resources/views/dropshipper-views/partials/_dashboard-order-status.blade.php
@@ -1,5 +1,5 @@
 <div class="col-sm-6 col-lg-3">
-    <a class="order-stats order-stats_pending" href="{{route('vendor.orders.list',['pending'])}}">
+    <a class="order-stats order-stats_pending" href="{{route('dropshipper.orders.list',['pending'])}}">
         <div class="order-stats__content">
             <img width="20" src="{{dynamicAsset(path: 'public/assets/back-end/img/pending.png')}}" alt="">
             <h6 class="order-stats__subtitle">{{translate('pending')}}</h6>
@@ -8,7 +8,7 @@
     </a>
 </div>
 <div class="col-sm-6 col-lg-3">
-    <a class="order-stats order-stats_confirmed" href="{{route('vendor.orders.list',['confirmed'])}}">
+    <a class="order-stats order-stats_confirmed" href="{{route('dropshipper.orders.list',['confirmed'])}}">
         <div class="order-stats__content">
             <img width="20" src="{{dynamicAsset(path: 'public/assets/back-end/img/confirmed.png')}}" alt="">
             <h6 class="order-stats__subtitle">{{translate('confirmed')}}</h6>
@@ -17,7 +17,7 @@
     </a>
 </div>
 <div class="col-sm-6 col-lg-3">
-    <a class="order-stats order-stats_packaging" href="{{route('vendor.orders.list',['processing'])}}">
+    <a class="order-stats order-stats_packaging" href="{{route('dropshipper.orders.list',['processing'])}}">
         <div class="order-stats__content">
             <img width="20" src="{{dynamicAsset(path: 'public/assets/back-end/img/packaging.png')}}" alt="">
             <h6 class="order-stats__subtitle">{{translate('packaging')}}</h6>
@@ -26,7 +26,7 @@
     </a>
 </div>
 <div class="col-sm-6 col-lg-3">
-    <a class="order-stats order-stats_out-for-delivery" href="{{route('vendor.orders.list',['out_for_delivery'])}}">
+    <a class="order-stats order-stats_out-for-delivery" href="{{route('dropshipper.orders.list',['out_for_delivery'])}}">
         <div class="order-stats__content">
             <img width="20" src="{{dynamicAsset(path: 'public/assets/back-end/img/out-of-delivery.png')}}" alt="">
             <h6 class="order-stats__subtitle">{{translate('out_For_Delivery')}}</h6>
@@ -37,7 +37,7 @@
 
 
 <div class="ol-sm-6 col-lg-3">
-    <a class="order-stats order-stats_delivered" href="{{route('vendor.orders.list',['delivered'])}}">
+    <a class="order-stats order-stats_delivered" href="{{route('dropshipper.orders.list',['delivered'])}}">
         <div class="order-stats__content">
             <img width="20" src="{{dynamicAsset(path: 'public/assets/back-end/img/delivered.png')}}" alt="">
             <h6 class="order-stats__subtitle">{{translate('delivered')}}</h6>
@@ -46,7 +46,7 @@
     </a>
 </div>
 <div class="ol-sm-6 col-lg-3">
-    <a class="order-stats order-stats_canceled" href="{{route('vendor.orders.list',['canceled'])}}">
+    <a class="order-stats order-stats_canceled" href="{{route('dropshipper.orders.list',['canceled'])}}">
         <div class="order-stats__content">
             <img width="20" src="{{dynamicAsset(path: 'public/assets/back-end/img/canceled.png')}}" alt="">
             <h6 class="order-stats__subtitle">{{translate('canceled')}}</h6>
@@ -55,7 +55,7 @@
     </a>
 </div>
 <div class="ol-sm-6 col-lg-3">
-    <a class="order-stats order-stats_returned" href="{{route('vendor.orders.list',['returned'])}}">
+    <a class="order-stats order-stats_returned" href="{{route('dropshipper.orders.list',['returned'])}}">
         <div class="order-stats__content">
             <img width="20" src="{{dynamicAsset(path: 'public/assets/back-end/img/returned.png')}}" alt="">
             <h6 class="order-stats__subtitle">{{translate('returned')}}</h6>
@@ -64,7 +64,7 @@
     </a>
 </div>
 <div class="ol-sm-6 col-lg-3">
-    <a class="order-stats order-stats_failed" href="{{route('vendor.orders.list',['failed'])}}">
+    <a class="order-stats order-stats_failed" href="{{route('dropshipper.orders.list',['failed'])}}">
         <div class="order-stats__content">
             <img width="20" src="{{dynamicAsset(path: 'public/assets/back-end/img/failed-to-deliver.png')}}" alt="">
             <h6 class="order-stats__subtitle">{{translate('failed_To_Delivery')}}</h6>
